5690f5ee91327b762bb66ea02161b35f2623855d
JasonYapzx <jasonyapzx@gmail.com>
2023-01-14 16:34:46 +0800

    feat: Landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      custom1: ["Custom-1", "cursive"],
+      custom2: ["Custom-2", "serif"],
+    },
     extend: {},
   },
   plugins: [],
